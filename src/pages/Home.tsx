3a35c35162527d55409825d3a37b9c7c844aa5c0
jp9141joao <jp9141joao@gmail.com>
2024-10-24 23:00:09 -0300

    commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,10 +13,10 @@ export default function Home() {
 
         <div className="grid place-items-center">
           <div className="grid grid-cols-1">
-            <div className='grid place-items-center xxs11:my-16 xs:my-[8vw] 2xl:my-[11vw]'>
+            <div className='grid place-items-center xxs11:my-16 xs:my-[8vw] xl:my-[9vw] 2xl:my-[13vw]'>
               <div className='text-center'>
                 <div>
-                  <h1 className="flex xxs11:text-[26px] xxs10:text-[28px] xxs9:text-3xl xxs8:text-[32px] xxs7:text-4xl xxs6:text-[40px] xxs5:text-[44px] xxs4:text-5xl xxs3:text-[52px] xxs2:text-[56px] xxs:text-[60px] xs:text-4xl text-gray-900 tracking-tight 2xl:text-8xl xl:text-7xl md:text-6xl sm:text-5xl grid leading-tight xs:flex xxs11:justify-center">
+                  <h1 className="flex xxs11:text-[26px] xxs10:text-[28px] xxs9:text-3xl xxs8:text-[32px] xxs7:text-4xl xxs6:text-[40px] xxs5:text-[44px] xxs4:text-5xl xxs3:text-[52px] xxs2:text-[56px] xxs:text-[60px] xs:text-4xl text-gray-900 tracking-tight 2xl:text-8xl 3xl:text-9xl xl:text-7xl md:text-6xl sm:text-5xl grid leading-tight xs:flex xxs11:justify-center">
                     <span className="block">
                       Let's planning&nbsp;
                     </span>
@@ -26,7 +26,7 @@ export default function Home() {
                   </h1>
                 </div>
                 <div className='xxs11:w-[200px] xxs10:w-[220px] xxs9:w-[240px] xxs8:w-[250px] xxs7:w-[270px] xxs6:w-[280px] xxs5:w-[290px] xxs4:w-[300px] xxs3:w-[310px] xxs2:w-[320px] xxs:w-[330px] xs:w-full xxs:flex xxs:justify-center xxs10:mt-2 xxs8:mt-4 xxs5:mt-5 xs:mt-1 sm:mt-3 2xl:mt-6'>
-                  <p className='xxs11:text-sm xxs6:text-sm xxs3:text-md xxs2:text-base xxs:text-lg xs:text-[12px] leading-tight text-gray-900 tracking-tight 2xl:text-2xl xl:text-lg md:text-md sm:text-sm'>
+                  <p className='xxs11:text-sm xxs6:text-sm xxs3:text-md xxs2:text-base xxs:text-lg xs:text-[12px] leading-tight text-gray-900 tracking-tight 3xl:text-3xl 2xl:text-2xl xl:text-lg md:text-md sm:text-sm'>
                     <span className='block'>Explore, organize, and embark on a unique adventure with ease. </span>             
                   </p>
                 </div>
@@ -41,7 +41,7 @@ export default function Home() {
               </div>
             </div>
 
-            <div className="w-full h-[60vw] xxs2:mt-10 mb-[24vw] xl:mb-[10vw]">
+            <div className="w-full h-[60vw]">
               <img 
                 src={Image} 
                 className="w-full h-full" 
@@ -53,8 +53,10 @@ export default function Home() {
         {/*
         grid place-items-center xxs11:mt-64 xxs10:mt-60 xxs9:mt-56 xxs8:mt-52 xxs7:mt-48 xxs6:mt-[140px] xxs5:mt-36 xxs4:mt-[124px] xxs3:mt-24 xxs2:mt-32 xxs:mt-28 w-full 2xl:mt-40
         */}
-        <div className="grid place-items-center">
-          <Credits/>
+        <div className="mt-auto">
+          <div className="text-center relative bottom-0 w-full">
+            <Credits />
+          </div>
         </div>
       </div>
     </>

bfbc25f7b81cb47f9aa267912be0e83bf6520cc6
jp9141joao <jp9141joao@gmail.com>
2024-10-30 23:53:48 -0300

    commit

--- src/components/login/SignIn.tsx
+++ src/components/login/SignIn.tsx
@@ -13,8 +13,19 @@ import Credits from "../Credits";
 import { useEffect, useState } from "react";
 
 
+{
+/*
+style={{border:"1px solid red"}}
+*/
+}
 
 export default function SignIn () {
+
+    const [w, setW ] = useState<number>(window.innerWidth);
+
+    useEffect(() => {
+        setW(window.innerWidth)
+    }, [window.innerWidth]);
 
     return (
         <>
@@ -25,17 +36,17 @@ export default function SignIn () {
 
                 <div className="flex min-h-screen flex-col">
                     <div className="grid grid-cols-1 flex-grow">
-                        <div className="w-full h-[60vw] ">
+                        <div className="grid-place-items-center">
                             <img 
                                 src={Image} 
-                                className="w-full h-full" 
+                                className="w-full h-full xs:px-[3vw]" 
                                 style={{ objectFit: "contain" }} 
                             />
                         </div>
 
-                        <div className="grid place-items-center gap-y-2 text-start xxs:mb-[8vh] xs:-mt-[40vh] mb- sm:my-[12vh] md:my-[14vh] lg:my-[13vh] xl:my-[10vh] 2xl:my-[7vw]">
-                            <div className="text-center">
-                                <div className="text-center leading-tight gap-y-0 text-gray-900 tracking-tight">
+                        <div className="grid place-items-center m-0 2xl:pb-0 gap-y-2 text-start xxs:my-14 xs:my-11 sm:mt-10 sm:pb-28 md:pb-32 md:mt-14 lg:my-28 lg:mb-0 xl:mt-56 xl:mb-20 2xl:my-28">
+                            <div className="">
+                                <div className="text-center place-items-center leading-tight gap-y-0 text-gray-900 tracking-tight">
                                     <p className="xxs11:text-sm xxs6:text-sm xxs3:text-md xxs2:text-xl xxs2:text-2xl xs:text-[18px] sm:text-lg md:text-xl xl:text-2xl 2xl:text-3xl">Welcome to</p>
                                     <h1 className="3xl:underline xxs11:grid text-center xs:flex xs:justify-center xxs11:text-[26px] xxs10:text-[28px] xxs9:text-3xl xxs8:text-[32px] xxs7:text-4xl xxs6:text-8xl xs:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl xxs2:-mt-3 xx2:-mt-4 xs:-mt-2 lg:-mt-3 2xl:-mt-4">
                                         <span className="block">Easy</span>
@@ -46,8 +57,8 @@ export default function SignIn () {
                                         Let's sign in to your account.
                                     </p>
                                 </div>
-                                <div>
-                                    <div className="grid place-items-center gap-y-4 xxs11:w-[200px] xxs10:w-[220px] xxs9:w-[240px] xxs8:w-[250px] xxs7:w-[270px] xxs6:w-[280px] xxs5:w-[290px] xxs4:w-[300px] xxs3:w-[310px] xxs2:w-[320px] xxs:w-[350px] xs:w-[270px] sm:w-[300px] md:w-[350px] lg:w-[400px] xl:w-[450px] 2xl:w-[500px]">
+                                <div className="xxs2:mt-3 xs:mt-2 md:mt-3 xl:mt-4">
+                                    <div className="grid gap-y-4 xxs11:w-[200px] xxs10:w-[220px] xxs9:w-[240px] xxs8:w-[250px] xxs7:w-[270px] xxs6:w-[280px] xxs5:w-[290px] xxs4:w-[300px] xxs3:w-[310px] xxs2:w-[320px] xxs:w-[350px] xs:w-[270px] sm:w-[300px] md:w-[350px] lg:w-[400px] xl:w-[450px] 2xl:w-[500px]">
                                         <div className="grid gap-1.5 w-full ">
                                             <Label htmlFor="email">Email</Label>
                                             <Input type="email" id="email" placeholder="name@example.com" />
@@ -61,7 +72,7 @@ export default function SignIn () {
                                         </div>
                                         <div className="flex items-center gap-1.5 w-full">
                                             <Checkbox id="remember-me"/>
-                                            <Label htmlFor="remember-me">Remember for 30 days</Label>
+                                            <Label htmlFor="remember-me">Remember for 30 days | {w}px</Label>
                                         </div>
                                         <div className="mt-2 grid gap-1.5 w-full">
                                             <Button type="submit">Sign in</Button>

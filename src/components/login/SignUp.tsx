bb83b9fefb62d1010914faca37159176c140f013
jp9141joao <162065477+jp9141joao@users.noreply.github.com>
2024-11-01 15:07:55 +0000

    commit

--- src/components/login/SignUp.tsx
+++ src/components/login/SignUp.tsx
@@ -10,67 +10,107 @@ import { CircleHelp } from "lucide-react"
 export default function SignIn(){
 
     return (
-        <div className="grid grid-cols-1 place-items-center transi">
-            <div className="grid place-items-center xl:mb-36 2xl:mt-[16vh] 2xl:mb-[10vh]">
-                <div className="grid text-center leading-tight xl:mb-3">
-                    <p className="text-center text-2xl">
-                        Join the  family of
-                    </p>
-                    <h1 className="text-center text-8xl xl:-mt-4 xl:mb-4">
-                        Easy Trip!
-                    </h1>
-                    <p className="text-center text-2xl">
-                        Sign up and explore the world.
-                    </p>
-                </div>
-                <div className="grid place-items-center xxs:mt-4 md:mt-6 xl:mt-8 gap-y-4">
-                    <div className="flex xxs:w-[350px] xs:w-[270px] sm:w-[300px] md:w-[350px] lg:w-[400px] xl:w-[450px] 2xl:w-[500px] items-center gap-1.5">
-                            <div className="grid w-full items-center">
-                                <Label htmlFor="name">Your name</Label>
-                                <Input type="text" id="phone" placeholder="Your name" />
-                            </div>
-                            <div className="grid w-full items-center">
-                                <Label htmlFor="lastName">Last name</Label>
-                                <Input type="text" id="phone" placeholder="Your last name" />
-                            </div>
+        <>
+            <div className="">
+                <GoBack />
+            </div>
+
+            <div className="grid grid-cols-1 place-items-center transi">
+                <div className="grid place-items-center xl:mb-36 2xl:mt-[16vh] 2xl:mb-[10vh]">
+                    <div className="grid text-center leading-tight xl:mb-3">
+                        <p className="text-center text-2xl">
+                            Join the  family of
+                        </p>
+                        <h1 className="text-center text-8xl xl:-mt-4 xl:mb-4">
+                            Easy Trip!
+                        </h1>
+                        <p className="text-center text-2xl">
+                            Sign up and explore the world.
+                        </p>
                     </div>
-                    <div className="flex xxs:w-[350px] xs:w-[270px] sm:w-[300px] md:w-[350px] lg:w-[400px] xl:w-[450px] 2xl:w-[500px] items-center gap-1.5">
-                            <div className="grid w-full items-center">
-                                <Label htmlFor="date">Your birth date</Label>
-                                <DatePicker/>
-                            </div>
-                            <div className="grid w-full items-center">
-                                <Label htmlFor="phone">Phone</Label>
-                                <Input type="text" id="phone" placeholder="Your Phone number" />
-                            </div>
+                    <div className="grid place-items-center xxs:mt-4 md:mt-6 xl:mt-8 gap-y-4">
+                        <div className="flex xxs:w-[350px] xs:w-[270px] sm:w-[300px] md:w-[350px] lg:w-[400px] xl:w-[450px] 2xl:w-[500px] items-center gap-1.5">
+                                <div className="grid w-full items-center">
+                                    <Label htmlFor="name">Your name</Label>
+                                    <Input 
+                                        type="text" 
+                                        id="phone" 
+                                        placeholder="Your name" 
+                                    />
+                                </div>
+                                <div className="grid w-full items-center">
+                                    <Label htmlFor="lastName">Last name</Label>
+                                    <Input 
+                                        type="text" 
+                                        id="lastName" 
+                                        placeholder="Your last name"
+                                    />
+                                </div>
+                        </div>
+                        <div className="flex xxs:w-[350px] xs:w-[270px] sm:w-[300px] md:w-[350px] lg:w-[400px] xl:w-[450px] 2xl:w-[500px] items-center gap-1.5">
+                                <div className="grid w-full items-center">
+                                    <Label htmlFor="date">
+                                        Your birth date
+                                    </Label>
+                                    <DatePicker/>
+                                </div>
+                                <div className="grid w-full items-center">
+                                    <Label htmlFor="phone">
+                                        Phone
+                                        </Label>
+                                    <Input 
+                                        type="text" 
+                                        id="phone" 
+                                        placeholder="Your Phone number" 
+                                    />
+                                </div>
+                        </div>
+                        <div className="grid xxs:w-[350px] xs:w-[270px] sm:w-[300px] md:w-[350px] lg:w-[400px] xl:w-[450px] 2xl:w-[500px] items-center gap-1.5">
+                            <Label htmlFor="email">
+                                Email
+                            </Label>
+                            <Input 
+                                type="email" 
+                                id="email" 
+                                placeholder="name@example.com"
+                            />
+                        </div>
+                        <div className="grid xxs:w-[350px] xs:w-[270px] sm:w-[300px] md:w-[350px] lg:w-[400px] xl:w-[450px] 2xl:w-[500px] items-center gap-1.5">
+                            <Label htmlFor="password">
+                                Password
+                            </Label>
+                            <Input type="password" id="password" placeholder="Abc123" />
+                        </div>
+                        <div className="mt-2 grid xxs:w-[350px] xs:w-[270px] sm:w-[300px] md:w-[350px] lg:w-[400px] xl:w-[450px] 2xl:w-[500px] items-center gap-1.5">
+                            <Button type="submit">
+                                Sign in
+                            </Button>
+                        </div>
+                        <div className="flex justify-center gap-2">
+                            <p>
+                                Have a account?
+                            </p>
+                            <Link to={"/login"}>
+                                <a className="underline">
+                                    Sign in!
+                                </a>
+                            </Link>
+                        </div>
                     </div>
-                    <div className="grid xxs:w-[350px] xs:w-[270px] sm:w-[300px] md:w-[350px] lg:w-[400px] xl:w-[450px] 2xl:w-[500px] items-center gap-1.5">
-                        <Label htmlFor="email">Email</Label>
-                        <Input type="email" id="email" placeholder="name@example.com" />
-                    </div>
-                    <div className="grid xxs:w-[350px] xs:w-[270px] sm:w-[300px] md:w-[350px] lg:w-[400px] xl:w-[450px] 2xl:w-[500px] items-center gap-1.5">
-                        <Label htmlFor="password">Password</Label>
-                        <Input type="password" id="password" placeholder="Abc123" />
-                    </div>
-                    <div className="mt-2 grid xxs:w-[350px] xs:w-[270px] sm:w-[300px] md:w-[350px] lg:w-[400px] xl:w-[450px] 2xl:w-[500px] items-center gap-1.5">
-                        <Button type="submit">Sign in</Button>
-                    </div>
-                    <div className="flex justify-center gap-2">
-                        <p>Have a account? </p>
-                        <Link to={"/login"}>
-                            <a className="underline">Sign in!</a>
-                        </Link>
-                    </div>
-                </div>
-            </div>    
-            <div className="w-full h-[36vw] mb-[10vh]">
-                    <img 
-                        src={Image} 
-                        className="w-full h-full" 
-                        style={{ objectFit: "contain" }} 
-                    />
-                </div>        
-        </div>
+                </div>    
+                <div className="w-full h-[36vw] mb-[10vh]">
+                        <img 
+                            src={Image} 
+                            className="w-full h-full" 
+                            style={{ objectFit: "contain" }} 
+                        />
+                    </div>        
+            </div>
+
+            <div className="">
+                <Credits />
+            </div>
+        </>
     )
 
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
         <div className="flex min-h-screen flex-col">
           <div className="grid grid-cols-1 flex-grow">
-            <div className='grid place-items-center xxs11:my-[9vh] xs:my-[12vh] xl:my-[9vw] 2xl:my-[13vw]'>
+            <div className='grid place-items-center xxs11:my-[9vh] xs:my-[12vh] xl:my-[9vw] 2xl:my-[9vw]'> {/*2xl:my-[13vw]*/}
               <div className='text-center'>
                 <div>
                   <h1 className="flex xxs11:text-[26px] xxs10:text-[28px] xxs9:text-3xl xxs8:text-[32px] xxs7:text-4xl xxs6:text-[40px] xxs5:text-[44px] xxs4:text-5xl xxs3:text-[52px] xxs2:text-[56px] xxs:text-[60px] xs:text-4xl text-gray-900 tracking-tight 2xl:text-8xl 3xl:text-9xl xl:text-7xl md:text-6xl sm:text-5xl grid leading-tight xs:flex xxs11:justify-center">
@@ -27,7 +27,9 @@ export default function Home() {
                 </div>
                 <div className='xxs11:w-[200px] xxs10:w-[220px] xxs9:w-[240px] xxs8:w-[250px] xxs7:w-[270px] xxs6:w-[280px] xxs5:w-[290px] xxs4:w-[300px] xxs3:w-[310px] xxs2:w-[320px] xxs:w-[330px] xs:w-full xxs:flex xxs:justify-center xxs10:mt-2 xxs8:mt-4 xxs5:mt-5 xs:mt-1 sm:mt-3 2xl:mt-6'>
                   <p className='xxs11:text-sm xxs6:text-sm xxs3:text-md xxs2:text-base xxs:text-lg xs:text-[12px] leading-tight text-gray-900 tracking-tight 3xl:text-3xl 2xl:text-2xl xl:text-lg md:text-md sm:text-sm'>
-                    <span className='block'>Explore, organize, and embark on a unique adventure with ease. </span>             
+                    <span className='block'>
+                      Explore, organize, and embark on a unique adventure with ease.
+                    </span>             
                   </p>
                 </div>
                 <div className="grid xxs11:gap-y-2 xxs9:gap-y-3 xxs7:gap-y-4 xs:flex xxs11:mt-3 xs:mt-1 sm:mt-2 2xl:mt-4 gap-5 sm:justify-center">

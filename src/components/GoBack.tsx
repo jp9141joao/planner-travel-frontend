c99b5b885b4031dc2cb9f3c3c5a4ddbfacc3eac1
Joao Pedro Rosa de Paula <162065477+jp9141joao@users.noreply.github.com>
2024-12-13 18:29:13 +0000

    commit

--- src/components/GoBack.tsx
+++ src/components/GoBack.tsx
@@ -8,7 +8,7 @@ export function GoBack(){
             <nav className="md:text-base lg:text-lg xl:text-xl my-3 ml-5 text-gray-900">
                 <Link to={'/home'}>
                     <h2 className="flex items-center gap-1 hover:-translate-x-1 transition-all">
-                        <MoveLeft className=""/>
+                        <MoveLeft className="mt-[0.2vw]"/>
                         Go back
                     </h2>
                 </Link>

6d591e2d694074e3abc0051e89ad5634bd3f0147
jp336joao <jp336joao@gmail.com>
2024-12-18 14:33:19 +0000

    commit

--- src/pages/resetPassword.tsx
+++ src/pages/resetPassword.tsx
@@ -4,7 +4,7 @@ import { BodyPage, BottomPage, MiddlePage, MiddlePageOneCol, TopPage } from "@/c
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Link } from "react-router-dom";
+import Image from '../assets/undraw_forgot-password_odai_inverted.svg'
 
 export function ResetPassword() {
     return (
@@ -12,21 +12,21 @@ export function ResetPassword() {
             <TopPage>
                 <GoBack to="signIn"/>
             </TopPage>
-            <MiddlePageOneCol>
-                <div className='mt-[3vw] xxs3:mt-[10vw] xs:mt-[2vw] sm:mt-[7vw] lg:mt-0 lg:mb-[3vw]'>
+            <MiddlePage>
+                <div className='mt-[5vw] xxs3:mt-[10vw] xs:mt-[2vw] sm:mt-[7vw] lg:mt-0 lg:mb-[3vw]' >
                     <div className="grid text-center place-items-center leading-tight gap-y-0 text-gray-900 tracking-tight">
-                            <div>
-                                <h1 className="grid text-[14.2vw] xxs5:text-[20.8vw] xxs3:text-[21.2vw] xs:text-[15vw] lg:text-[4vw] w-full text-gray-900 tracking-tight leading-[1]">
-                                    Password Reset
-                                </h1>
-                            </div>
-                            <div>
-                                <p className="xs:text-start text-[8vw] xxs8:text-[6.8vw] xs:text-[4.75vw] lg:text-[1.8vw] mt-[6.2vw] xxs5:mt-[7.8vw] xs:mt-[5.7vw] lg:mt-[0.6vw] leading-tight text-gray-900 tracking-tight">
-                                    Create a new password.
-                                </p>
-                            </div>
+                        <div>
+                            <h1 className="grid text-[14.2vw] xxs5:text-[13.5vw] xs:text-[9.4vw] lg:text-[4.15vw] w-full text-gray-900 tracking-tight leading-[1]">
+                                Password Reset
+                            </h1>
+                        </div>
+                        <div>
+                            <p className="xs:text-start text-[8vw] xxs5:text-[7.8vw] xs:text-[5.5vw] lg:text-[2vw] xxs5:mt-[0.8vw] xs:mt-[0.6vw] lg:mt-[0.6vw] leading-tight text-gray-900 tracking-tight">
+                                Create a new password.
+                            </p>
+                        </div>
                     </div>
-                    <div className="grid place-items-center gap-y-1 xxs3:gap-y-2 xxs11:mt-[3vw] xs:mt-3 md:mt-4 xl:mt-[0.7vw] px-0 w-full" >
+                    <div className="grid place-items-center gap-y-1 xxs3:gap-y-2 xxs11:mt-[3vw] xxs3:mt-[5vw] xs:mt-3 md:mt-4 xl:mt-[0.7vw] px-0 w-full" >
                         <div className="grid gap-1.5 w-full place-items-start">
                             <Label htmlFor="email" className="text-[4vw] xxs5:text-sm sm:text-base lg:text-lg">
                                 Email
@@ -65,7 +65,13 @@ export function ResetPassword() {
                         
                     </div>
                 </div>
-            </MiddlePageOneCol>
+                <div   className="mx-[14vw] xxs3:mx-[6vw] xs:mx-[22vw] sm:mx-[20vw] lg:mx-[2vw] mt-[5vw] xxs5:mt-[2.4vw] xs:mt-[1vw]">
+                    <img
+                        src={Image}
+                        className="w-auto h-auto"
+                    />
+                </div>
+            </MiddlePage>
             <BottomPage>
                 <Credits/>
             </BottomPage>
